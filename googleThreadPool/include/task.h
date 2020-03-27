6fd1d6cb14841efb72a6e8f03743a9bf8a3f2f1b
zhenwei.li <lizhenwei@chinaredstar.cn>
2020-03-27 18:17:15 +0800

    增加注释

--- googleThreadPool/include/task.h
+++ googleThreadPool/include/task.h
@@ -10,49 +10,57 @@
 class ThreadPoolInterface;
 
 class Task {
- public:
-  friend class ThreadPoolInterface;
+public:
+    friend class ThreadPoolInterface;
 
-  using WorkItem = std::function<void()>;
-  enum State { NEW, DISPATCHED, DEPENDENCIES_COMPLETED, RUNNING, COMPLETED };
+    using WorkItem = std::function<void()>;
+    enum State { NEW, DISPATCHED, DEPENDENCIES_COMPLETED, RUNNING, COMPLETED };
 
-  Task() = default;
-  ~Task();
+    Task() = default;
+    ~Task();
 
-  State GetState() LOCKS_EXCLUDED(mutex_);
+    State GetState() LOCKS_EXCLUDED(mutex_);  // 返回本Task当前状态
 
-  // State must be 'NEW'.
-  void SetWorkItem(const WorkItem& work_item) LOCKS_EXCLUDED(mutex_);
+    // State must be 'NEW'.
+    void SetWorkItem(const WorkItem& work_item) LOCKS_EXCLUDED(mutex_);  // 设置Task 执行的任务 （函数）
 
-  // State must be 'NEW'. 'dependency' may be nullptr, in which case it is
-  // assumed completed.
-  void AddDependency(std::weak_ptr<Task> dependency) LOCKS_EXCLUDED(mutex_);
-  void AddTaskInfo(const std::string& task_info, const int type=0) LOCKS_EXCLUDED(mutex_);
-  std::string getTaskInfo() LOCKS_EXCLUDED(mutex_);
+    // State must be 'NEW'. 'dependency' may be nullptr, in which case it is
+    // assumed completed.
+    // 给当前任务添加 依赖任务，如当前任务为b，添加依赖任务为a（a——>b: b.AddDependency(a))
+    // 同时并把当前任务b，加入到依赖任务a的dependent_tasks_列表中，以便执行a后，对应更改b的状态）。
+    void AddDependency(std::weak_ptr<Task> dependency) LOCKS_EXCLUDED(mutex_);
+    void AddTaskInfo(const std::string& task_info, const int type=0) LOCKS_EXCLUDED(mutex_);
+    std::string getTaskInfo() LOCKS_EXCLUDED(mutex_);
 
- private:
-  // Allowed in all states.
-  void AddDependentTask(Task* dependent_task);
+private:
+    // Allowed in all states.
+    // AddDependency 功能具体实现函数
+    // 添加依赖本Task的Task，如b依赖a，则a-->b， a.AddDependentTask(b), 根据a的状态，改变b的状态
+    // 如果a完成，则b的依赖-1；（并把当前任务b，加入到依赖任务a的 dependent_tasks_ 列表中，以便执行a后，对应更改b的状态）。
+    void AddDependentTask(Task* dependent_task);
 
-  // State must be 'DEPENDENCIES_COMPLETED' and becomes 'COMPLETED'.
-  void Execute() LOCKS_EXCLUDED(mutex_);
-  void  Remove()  LOCKS_EXCLUDED(mutex_);
-  // State must be 'NEW' and becomes 'DISPATCHED' or 'DEPENDENCIES_COMPLETED'.
-  void SetThreadPool(ThreadPoolInterface* thread_pool) LOCKS_EXCLUDED(mutex_);
+    // State must be 'DEPENDENCIES_COMPLETED' and becomes 'COMPLETED'.
+    // 执行当前任务，比如当前任务为a，并依此更新依赖a的任务dependent_tasks_中所有任务状态，如依赖a的b。
+    void Execute() LOCKS_EXCLUDED(mutex_);
+    void  Remove()  LOCKS_EXCLUDED(mutex_);
+    // State must be 'NEW' and becomes 'DISPATCHED' or 'DEPENDENCIES_COMPLETED'.
+    // 当前任务进入线程待执行队列
+    void SetThreadPool(ThreadPoolInterface* thread_pool) LOCKS_EXCLUDED(mutex_);
 
-  // State must be 'NEW' or 'DISPATCHED'. If 'DISPATCHED', may become
-  // 'DEPENDENCIES_COMPLETED'.
-  void OnDependenyCompleted();
+    // State must be 'NEW' or 'DISPATCHED'. If 'DISPATCHED', may become
+    // 'DEPENDENCIES_COMPLETED'.
+    // 当前任务的依赖任务完成时候，当前任务状态随之改变
+    void OnDependenyCompleted();
 
-  WorkItem work_item_ GUARDED_BY(mutex_);
-  ThreadPoolInterface* thread_pool_to_notify_ GUARDED_BY(mutex_) = nullptr;
-  State state_ GUARDED_BY(mutex_) = NEW;
-  unsigned int uncompleted_dependencies_ GUARDED_BY(mutex_) = 0;
-  std::set<Task*> dependent_tasks_ GUARDED_BY(mutex_);
+    WorkItem work_item_ GUARDED_BY(mutex_);  // 任务具体执行过程
+    ThreadPoolInterface* thread_pool_to_notify_ GUARDED_BY(mutex_) = nullptr;  // 执行当前任务的线程池
+    State state_ GUARDED_BY(mutex_) = NEW;  // 初始化状态为 NEW
+    unsigned int uncompleted_dependencies_ GUARDED_BY(mutex_) = 0;  // 当前任务依赖的任务的数量
+    std::set<Task*> dependent_tasks_ GUARDED_BY(mutex_);  // 依赖当前任务的任务列表
 
-  std::chrono::steady_clock::time_point add_time_;
-  std::string info_ GUARDED_BY(mutex_);
-  int         type_ GUARDED_BY(mutex_);//0:default; 1:create fast matcher; 2: local constrain; 3: global constrain 4: finish one node; 5: spa
-  absl::Mutex mutex_;
+    std::chrono::steady_clock::time_point add_time_;
+    std::string info_ GUARDED_BY(mutex_);
+    int         type_ GUARDED_BY(mutex_);//0:default; 1:create fast matcher; 2: local constrain; 3: global constrain 4: finish one node; 5: spa
+    absl::Mutex mutex_;
 };
 #endif
--- googleThreadPool/include/thread_pool.h
+++ googleThreadPool/include/thread_pool.h
@@ -15,19 +15,19 @@
 class Task;
 
 class ThreadPoolInterface {
- public:
-  ThreadPoolInterface() {}
-  virtual ~ThreadPoolInterface() {}
-  virtual std::weak_ptr<Task> Schedule(std::unique_ptr<Task> task) = 0;
+public:
+    ThreadPoolInterface() {}
+    virtual ~ThreadPoolInterface() {}
+    virtual std::weak_ptr<Task> Schedule(std::unique_ptr<Task> task) = 0;
 
- protected:
-  void Execute(Task* task);
-  void SetThreadPool(Task* task);
+protected:
+    void Execute(Task* task);
+    void SetThreadPool(Task* task);
 
- private:
-  friend class Task;
+private:
+    friend class Task;
 
-  virtual void NotifyDependenciesCompleted(Task* task) = 0;
+    virtual void NotifyDependenciesCompleted(Task* task) = 0;
 };
 
 // A fixed number of threads working on tasks. Adding a task does not block.
@@ -37,28 +37,29 @@ class ThreadPoolInterface {
 // destructor. The thread pool will then wait for the currently executing work
 // items to finish and then destroy the threads.
 class ThreadPool : public ThreadPoolInterface {
- public:
-  explicit ThreadPool(int num_threads);
-  ~ThreadPool();
+public:
+    explicit ThreadPool(int num_threads);  // 初始化一个线程数量固定的线程池
+    ~ThreadPool();
 
-  ThreadPool(const ThreadPool&) = delete;
-  ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
 
-  // When the returned weak pointer is expired, 'task' has certainly completed,
-  // so dependants no longer need to add it as a dependency.
-  std::weak_ptr<Task> Schedule(std::unique_ptr<Task> task)
-      LOCKS_EXCLUDED(mutex_) override;
+    // When the returned weak pointer is expired, 'task' has certainly completed,
+    // so dependants no longer need to add it as a dependency.
+    std::weak_ptr<Task> Schedule(std::unique_ptr<Task> task)
+        LOCKS_EXCLUDED(mutex_) override;  // 添加想要ThreadPool执行的task，插入tasks_not_ready_,
+    // 如果任务满足执行要求，直接插入task_queue_准备执行
 
- private:
-  void DoWork();
-  void DoWork(const int thread_id);
-  void NotifyDependenciesCompleted(Task* task) LOCKS_EXCLUDED(mutex_) override;
+private:
+    void DoWork(); // 每个线程初始化时,执行DoWork()函数. 与线程绑定
+    void DoWork(const int thread_id);
+    void NotifyDependenciesCompleted(Task* task) LOCKS_EXCLUDED(mutex_) override;
 
-  absl::Mutex mutex_;
-  bool running_ GUARDED_BY(mutex_) = true;
-  std::vector<std::thread> pool_ GUARDED_BY(mutex_);
-  std::deque<std::shared_ptr<Task>> task_queue_ GUARDED_BY(mutex_);
-  absl::flat_hash_map<Task*, std::shared_ptr<Task>> tasks_not_ready_
-      GUARDED_BY(mutex_);
+    absl::Mutex mutex_;
+    bool running_ GUARDED_BY(mutex_) = true;  // running_只是一个监视哨,只有线程池在running_状态时,才能往work_queue_加入函数.
+    std::vector<std::thread> pool_ GUARDED_BY(mutex_);
+    std::deque<std::shared_ptr<Task>> task_queue_ GUARDED_BY(mutex_);  // 准备执行的task
+    absl::flat_hash_map<Task*, std::shared_ptr<Task>> tasks_not_ready_
+        GUARDED_BY(mutex_);  // 未准备好的 task，task可能有依赖还未完成
 };
 #endif
